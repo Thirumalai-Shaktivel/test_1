96771888ce389d3df8d698f4afa135ba9943e3a5
Ondřej Čertík <ondrej@certik.us>
2021-09-09 09:49:23 -0600

    Remove b

--- build.sh
+++ build.sh
@@ -4,6 +4,4 @@ set -ex
 
 gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf.f90 -o sin_perf
 
-gfortran -O3 -march=native -funroll-loops -c b.f90 -o b.o
-gfortran -O3 -march=native -funroll-loops -ffast-math -c sin_perf_pure.f90 -o sin_perf_pure.o
-gfortran -flto -o sin_perf_pure sin_perf_pure.o b.o
+gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf_pure.f90 -o sin_perf_pure
