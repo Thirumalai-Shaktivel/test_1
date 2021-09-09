5e65096ba8a5e70d4044372031b009bd6f49622d
Ondřej Čertík <ondrej@certik.us>
2021-09-09 16:08:30 -0600

    Split into separate compilations

--- build.sh
+++ build.sh
@@ -6,4 +6,6 @@ gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf.f90 -o sin_perf
 
 gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf_pure.f90 -o sin_perf_pure
 
-gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf_pure_vec.f90 -o sin_perf_pure_vec
+gfortran -O3 -march=native -funroll-loops -ffast-math -c sin_perf_pure_vec2.f90 -o sin_perf_pure_vec2.o
+gfortran -O3 -march=native -funroll-loops -ffast-math -c sin_perf_pure_vec.f90 -o sin_perf_pure_vec.o
+gfortran -O3 -march=native -funroll-loops -ffast-math -flto -o sin_perf_pure_vec sin_perf_pure_vec.o sin_perf_pure_vec2.o
