3d71d064a3253f71c5e4bce7f3e5b67a09acf672
Ondřej Čertík <ondrej@certik.us>
2021-09-09 09:52:48 -0600

    speedup

--- sin_perf_pure.f90
+++ sin_perf_pure.f90
@@ -85,15 +85,15 @@ call renormalize(zh, zl, xh, yh)
 r = zh+(zl+yl)
 end function
 
-subroutine dd_mul(zh, zl, xh, xl, yh, yl)
+subroutine dd_mul(zh, zl, xh, yh, yl)
 real(dp), intent(out) :: zh, zl
-real(dp), intent(in) :: xh, xl, yh, yl
+real(dp), intent(in) :: xh, yh, yl
 real(dp) :: zh0, zl0, u1, u2, v1, v2
 call split(u1, u2, xh)
 call split(v1, v2, yh)
 zh0 = xh*yh
 zl0 = (((u1*v1-zh0)+(u1*v2))+(u2*v1))+(u2*v2)
-zl0 = zl0 + xh*yl + xl*yh
+zl0 = zl0 + xh*yl
 call renormalize(zh, zl, zh0, zl0)
 end subroutine
 
@@ -123,7 +123,7 @@ if (abs(xh) < 1e16) then
     yh = 6.283185307179586_dp ! 2*pi (high)
     yl = 2.4492935982947064e-16_dp ! 2*pi (low)
     N = floor2(xh/yh)
-    call dd_mul(zh, zl, -real(N,dp), 0._dp, yh, yl)
+    call dd_mul(zh, zl, -real(N,dp), yh, yl)
     zh = dd_add1(xh, zh, zl)
 else
     error stop "unsupported range"
