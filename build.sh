8e66c67f4df8e2a6e932b41bbef30615b8db176a
Ondřej Čertík <ondrej@certik.us>
2021-09-09 12:59:34 -0600

    Add a vectorized version

--- build.sh
+++ build.sh
@@ -5,3 +5,5 @@ set -ex
 gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf.f90 -o sin_perf
 
 gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf_pure.f90 -o sin_perf_pure
+
+gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf_pure_vec.f90 -o sin_perf_pure_vec
