2cacb573733516c0f3bafde10b02481000c31493
Ondřej Čertík <ondrej@certik.us>
2021-09-10 10:24:45 -0600

    Add a vectorized version

--- sin_perf_pure_vec.f90
+++ sin_perf_pure_vec.f90
@@ -1,6 +1,7 @@
 program sin_perf_pure_vec
 use, intrinsic :: iso_fortran_env, only: dp => real64, i8 => int64
-use sin_perf_pure_vec2, only: array_copy, pi, array_kernel_sin1
+use sin_perf_pure_vec2, only: array_copy, pi, array_kernel_sin1, &
+        array_kernel_sin2
 implicit none
 
 integer(i8), parameter :: sizes(*) = [ &
@@ -61,7 +62,8 @@ do j = 1, size(sizes)
     do k = 1, M
         !call array_copy(Ntile, x(k:k+Ntile-1), r(k:k+Ntile-1))
         !call array_copy(Ntile, x, r)
-        call array_kernel_sin1(Ntile, x, r)
+        !call array_kernel_sin1(Ntile, x, r)
+        call array_kernel_sin2(Ntile, x, r)
         !r(i) = x(i)
         !r(i) = kernel_dsin(x(i))
         !r(i) = dsin2(x(i))
--- sin_perf_pure_vec2.f90
+++ sin_perf_pure_vec2.f90
@@ -23,6 +23,14 @@ do i = 1, n
 end do
 end subroutine
 
+subroutine array_kernel_sin2(n, A, B)
+integer(i8), intent(in) :: n
+real(dp), intent(in) :: A(n)
+real(dp), intent(out) :: B(n)
+integer(i8) :: i
+call kernel_dsin2(n, A, B)
+end subroutine
+
 
 elemental real(dp) function dsin1(x) result(r)
 real(dp), intent(in) :: x
@@ -68,6 +76,23 @@ real(dp) :: z
 z = x*x
 res = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
 end function
+
+subroutine kernel_dsin2(n, x, R)
+integer(i8), intent(in) :: n
+real(dp), intent(in) :: x(n)
+real(dp), intent(out) :: R(n)
+real(dp), parameter :: S1 = 0.9999999999999990771_dp
+real(dp), parameter :: S2 = -0.16666666666664811048_dp
+real(dp), parameter :: S3 = 8.333333333226519387e-3_dp
+real(dp), parameter :: S4 = -1.9841269813888534497e-4_dp
+real(dp), parameter :: S5 = 2.7557315514280769795e-6_dp
+real(dp), parameter :: S6 = -2.5051823583393710429e-8_dp
+real(dp), parameter :: S7 = 1.6046585911173017112e-10_dp
+real(dp), parameter :: S8 = -7.3572396558796051923e-13_dp
+real(dp) :: z(n)
+z = x*x
+R = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
+end subroutine
 
 elemental integer(8) function floor2(x) result(r)
 real(dp), intent(in) :: x
