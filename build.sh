649dd5c4f32193c362169a550155ed5079900c53
Ondřej Čertík <ondrej@certik.us>
2021-09-12 13:06:43 -0600

    Benchmark array copy

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ set -ex
 #clang -O2 -c kernel_sin2.ll -o kernel_sin.o
 clang -O3 -funroll-loops -ffast-math -c kernel_sin3.c -o kernel_sin.o
 #gfortran -O3 -funroll-loops -ffast-math -c kernel_sin4.f90 -o kernel_sin.o
-#clang -c array_copy2.c -o array_copy2.o
+clang -O3 -funroll-loops -ffast-math -c array_copy2.c -o array_copy2.o
 
 gfortran -O3 -funroll-loops -ffast-math sin_perf.f90 -o sin_perf
 
@@ -20,5 +20,5 @@ gfortran -O3 -funroll-loops -ffast-math -c sin_perf_pure_vec2.f90 -o sin_perf_pu
 gfortran -O3 -funroll-loops -ffast-math -c sin_perf_pure_vec.f90 -o sin_perf_pure_vec.o
 gfortran -O3 -funroll-loops -ffast-math -flto \
     -o sin_perf_pure_vec sin_perf_pure_vec.o sin_perf_pure_vec2.o \
-    kernel_sin.o
-    #array_copy2.o array_read.o array_write.o
+    kernel_sin.o \
+    array_copy2.o #array_read.o array_write.o
