b4a82236d54295fbcfc3f2fa52497af57ceb99dc
Ondřej Čertík <ondrej@certik.us>
2021-09-11 22:41:48 -0600

    Update -march

--- build.sh
+++ build.sh
@@ -12,12 +12,12 @@ clang -O3 -march=native -funroll-loops -ffast-math -c kernel_sin3.c -o kernel_si
 gfortran -O3 -march=skylake -funroll-loops -ffast-math -c kernel_sin4.f90 -o kernel_sin.o
 #clang -c array_copy2.c -o array_copy2.o
 
-gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf.f90 -o sin_perf
+gfortran -O3 -march=skylake -funroll-loops -ffast-math sin_perf.f90 -o sin_perf
 
-gfortran -O3 -march=native -funroll-loops -ffast-math sin_perf_pure.f90 -o sin_perf_pure
+gfortran -O3 -march=skylake -funroll-loops -ffast-math sin_perf_pure.f90 -o sin_perf_pure
 
-gfortran -O3 -march=native -funroll-loops -ffast-math -c sin_perf_pure_vec2.f90 -o sin_perf_pure_vec2.o
-gfortran -O3 -march=native -funroll-loops -ffast-math -c sin_perf_pure_vec.f90 -o sin_perf_pure_vec.o
-gfortran -O3 -march=native -funroll-loops -ffast-math -flto \
+gfortran -O3 -march=skylake -funroll-loops -ffast-math -c sin_perf_pure_vec2.f90 -o sin_perf_pure_vec2.o
+gfortran -O3 -march=skylake -funroll-loops -ffast-math -c sin_perf_pure_vec.f90 -o sin_perf_pure_vec.o
+gfortran -O3 -march=skylake -funroll-loops -ffast-math -flto \
     -o sin_perf_pure_vec sin_perf_pure_vec.o sin_perf_pure_vec2.o \
     array_copy2.o array_read.o array_write.o kernel_sin.o
