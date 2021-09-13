00e79cf25e030f55bbb437ccbf8ddd76db55d68c
Ondřej Čertík <ondrej@certik.us>
2021-09-13 08:37:11 -0600

    Use +4

--- array_read2.asm
+++ array_read2.asm
@@ -6,14 +6,17 @@
 .p2align	2
 
 _array_read:
-;        mov x11, #8
-;        mul x0, x0, x11
+        mov x9, #0
+        add x10, x1, #0
+        add x11, x1, #1
+        add x12, x1, #2
+        add x13, x1, #3
 .main_loop:
-        ldr d0, [x1]
-        ldr d1, [x1, #8]
-        ldr d2, [x1, #16]
-        ldr d3, [x1, #24]
-        add x1, x1, #32
+        ldr d0, [x10, x9, lsl #3]
+        ldr d1, [x11, x9, lsl #3]
+        ldr d2, [x12, x9, lsl #3]
+        ldr d3, [x13, x9, lsl #3]
+        add x9, x9, #4
 ;        cmp x1, x0
         subs x0, x0, #4
         b.ne	.main_loop
