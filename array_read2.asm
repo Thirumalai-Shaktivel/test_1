70474cdf13f8feed6ff073dbcf71b0e56958bbae
Ondřej Čertík <ondrej@certik.us>
2021-09-12 21:48:56 -0600

    Add array read

---
+++ array_read2.asm
@@ -0,0 +1,18 @@
+; void array_read(long n, double *A);
+; Original, simplest version. ARM64 (M1)
+
+.section	__TEXT,__text,regular,pure_instructions
+.globl	_array_read
+.p2align	2
+
+_array_read:
+.main_loop:
+        ldr	d0, [x1]
+        ldr	d1, [x1, #8]
+        ldr	d2, [x1, #16]
+        ldr	d3, [x1, #24]
+        adds    x1, x1, #32
+        subs	x0, x0, #4
+        b.ne	.main_loop
+.epilog:
+        ret
