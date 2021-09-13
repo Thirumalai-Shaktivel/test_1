c61ca3f4a37a3846114bfd65e68bfa190b362633
Ondřej Čertík <ondrej@certik.us>
2021-09-13 08:29:47 -0600

    Prepare

--- array_read2.asm
+++ array_read2.asm
@@ -6,12 +6,14 @@
 .p2align	2
 
 _array_read:
+;        mov x11, #8
+;        mul x0, x0, x11
 .main_loop:
         ldr d0, [x1]
         ldr d1, [x1, #8]
         ldr d2, [x1, #16]
         ldr d3, [x1, #24]
-        adds x1, x1, #32
+        add x1, x1, #32
 ;        cmp x1, x0
         subs x0, x0, #4
         b.ne	.main_loop
