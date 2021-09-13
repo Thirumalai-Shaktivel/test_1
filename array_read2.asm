dfa51483cc6a1707600a3aba01f1e73788a2d278
Ondřej Čertík <ondrej@certik.us>
2021-09-13 12:31:30 -0600

    Work on read

--- array_read2.asm
+++ array_read2.asm
@@ -10,11 +10,15 @@ _array_read:
         mul x0, x0, x9
         add x0, x0, x1
 .main_loop:
-        ldr x14, [x1, 0]
-        ldr x15, [x1, 8]
-        ldr x8,  [x1, 16]
-        ldr x7,  [x1, 24]
-        add x1, x1, 32
+        ldr d0, [x1, 0]
+        ldr d1, [x1, 8]
+        ldr d2, [x1, 16]
+        ldr d3, [x1, 24]
+        ldr d4, [x1, 32]
+        ldr d5, [x1, 40]
+        ldr d6, [x1, 48]
+        ldr d7, [x1, 56]
+        add x1, x1, 64
         cmp x1, x0
         b.ne	.main_loop
 .epilog:
