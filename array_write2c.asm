41f266bfd2a2e3b4e36c7faff5872fd50b32c4e4
Ondřej Čertík <ondrej@certik.us>
2021-09-13 12:11:34 -0600

    Unroll 8 times

--- array_write2c.asm
+++ array_write2c.asm
@@ -12,11 +12,15 @@ _array_write:
 .main_loop:
         ; x1 ... pointer &B(i), we are processing B(i:i+3)
         ; x0 ... ending condition pointer, ends when x1 == x0
-        str q1, [x1,  0]
-        str q3, [x1, 16]
-        str q5, [x1, 32]
-        str q7, [x1, 48]
-        add x1, x1, 64
+        str q1, [x1,   0]
+        str q2, [x1,  16]
+        str q3, [x1,  32]
+        str q4, [x1,  48]
+        str q5, [x1,  64]
+        str q6, [x1,  80]
+        str q7, [x1,  96]
+        str q8, [x1, 112]
+        add x1, x1, 128
         cmp x1, x0
         b.ne	.main_loop
 .epilog:
