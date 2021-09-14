f254ef36eb3b415e4af02c3ea8b1b6328c0d4e9c
Ondřej Čertík <ondrej@certik.us>
2021-09-14 13:59:05 -0600

    Polish the documentation

--- Performance Vectorized.py
+++ Performance Vectorized.py
@@ -33,7 +33,7 @@ GHz = 1e9
 KB = 1024
 MB = 1024**2
 
-# 
+# Intel:
 # https://www.agner.org/optimize/instruction_tables.pdf
 # R: 0.125    (`VMOVAPS y,m256` 0.5 cycles per instruction = 4 doubles)
 # W: 0.25     (`VMOVAPS m256,y` 1 cycle per instruction = 4 doubles)
@@ -46,28 +46,32 @@ cpu_freq = 4.590 * GHz # Actual
 L1 = 64 * KB
 L2 = 256 * KB
 L3 = 16 * MB
-k = 8 *2  # 8 bytes per element, 2 arrays
+k = 8 * 2  # 8 bytes per element, 2 arrays
+
+# ARM:
 # https://www.techpowerup.com/cpu-specs/core-i9-10980hk.c2276
 # https://dougallj.github.io/applecpu/firestorm-simd.html
 #
 # Operation speeds Apple M1 (ARM64) per double
 #
-# W: 0.25 - 0.333 - 0.5   (Both `stp d0, d1, [x1]` and `str q1, [x1]` take 0.5 cycles; `stp q1, q2, [x1]` takes 1 cycle)
 # R: 0.1665 - 0.2 - 0.25 (`ldr q0, [x1]` takes 0.333)
+# W: 0.25 - 0.333 - 0.5   (Both `stp d0, d1, [x1]` and `str q1, [x1]` take 0.5 cycles; `stp q1, q2, [x1]` takes 1 cycle)
 # *: 0.125  (`fmul.2d v0, v0, v0` takes 0.25)
 # # +: 0.125  (`fadd.2d v0, v0, v0` takes 0.25)
 # fma: 0.125  (`fmla.2d v0, v0, v0` takes 0.25)
 #
-# Note: There are two units for R, one unit for W and one unit can do both. The first number in 0.25 - 0.333 - 0.5
-# is only writing, then read/write sharing 50% of the common unit, and the last number is only the 1 unit for  W.
-# Example: For array copy, we assume the unit gets used 50%, use the middle number and expect 0.333 for the peak.
+# Note: There are two units for R, one unit for W and one unit can do both. The
+# first number in 0.25 - 0.333 - 0.5 is only writing, then read/write sharing
+# 50% of the common unit, and the last number is only the 1 unit for  W.
+# Example: For array copy, we assume the unit gets used 50%, use the middle
+# number and expect 0.333 for the peak.
 #
-#cpu_freq = 2.4 * GHz
-cpu_freq = 3.2 * GHz
+#cpu_freq = 2.4 * GHz # Base
+cpu_freq = 3.2 * GHz # Boost
 L1 = 320 * KB
 L2 = 12 * MB
 #L3 = 16 * MB
-k = 8 * 2# 8 bytes per element, 2 arrays
+k = 8 * 2 # 8 bytes per element, 2 arrays
 
 figure(figsize=(20, 12))
 #loglog(x, sin_gf, ".", label="GFortran Intrinsic")
