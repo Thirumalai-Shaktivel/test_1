78c5c42462370383355aba6d80d5a5ba9a40d86b
Ondřej Čertík <ondrej@certik.us>
2021-09-14 15:54:56 -0600

    Interleave

--- array_mul4a.asm
+++ array_mul4a.asm
@@ -22,33 +22,35 @@ array_mul2:
         align 16
 .main_loop:
         ; Unroll n=4 times
-        vmovaps ymm0, [rsi+8*rax] ; load A(i:i+3)
-        vmulpd ymm0, ymm0, ymm0   ; x = x*x
-        vmulpd ymm0, ymm0, ymm0   ; x = x*x
-        vmulpd ymm0, ymm0, ymm0   ; x = x*x
-        vmulpd ymm0, ymm0, ymm0   ; x = x*x
-        vmovaps [rdx+8*rax], ymm0 ; store B(i:i+3)
-
+        vmovaps ymm0, [rsi+8*rax] ;1 load A(i:i+3)
         vmovaps ymm1, [rsi+8*(rax+4)] ;2
-        vmulpd ymm1, ymm1, ymm1   ;2
-        vmulpd ymm1, ymm1, ymm1   ;2
-        vmulpd ymm1, ymm1, ymm1   ;2
-        vmulpd ymm1, ymm1, ymm1   ;2
-        vmovaps [rdx+8*(rax+4)], ymm1 ;2
-
         vmovaps ymm2, [rsi+8*(rax+8)] ;3
-        vmulpd ymm2, ymm2, ymm2   ;3
-        vmulpd ymm2, ymm2, ymm2   ;3
-        vmulpd ymm2, ymm2, ymm2   ;3
-        vmulpd ymm2, ymm2, ymm2   ;3
-        vmovaps [rdx+8*(rax+8)], ymm2 ;3
+        vmovaps ymm3, [rsi+8*(rax+12)] ;4
 
-        vmovaps ymm3, [rsi+8*(rax+12)] ;3
-        vmulpd ymm3, ymm3, ymm3   ;3
-        vmulpd ymm3, ymm3, ymm3   ;3
-        vmulpd ymm3, ymm3, ymm3   ;3
-        vmulpd ymm3, ymm3, ymm3   ;3
-        vmovaps [rdx+8*(rax+12)], ymm3 ;3
+        vmulpd ymm0, ymm0, ymm0   ;1 x = x*x
+        vmulpd ymm1, ymm1, ymm1   ;2
+        vmulpd ymm2, ymm2, ymm2   ;3
+        vmulpd ymm3, ymm3, ymm3   ;4
+
+        vmulpd ymm0, ymm0, ymm0   ;1 x = x*x
+        vmulpd ymm1, ymm1, ymm1   ;2
+        vmulpd ymm2, ymm2, ymm2   ;3
+        vmulpd ymm3, ymm3, ymm3   ;4
+
+        vmulpd ymm0, ymm0, ymm0   ;1 x = x*x
+        vmulpd ymm1, ymm1, ymm1   ;2
+        vmulpd ymm2, ymm2, ymm2   ;3
+        vmulpd ymm3, ymm3, ymm3   ;4
+
+        vmulpd ymm0, ymm0, ymm0   ;1 x = x*x
+        vmulpd ymm1, ymm1, ymm1   ;2
+        vmulpd ymm2, ymm2, ymm2   ;3
+        vmulpd ymm3, ymm3, ymm3   ;4
+
+        vmovaps [rdx+8*rax], ymm0 ;1 store B(i:i+3)
+        vmovaps [rdx+8*(rax+4)], ymm1 ;2
+        vmovaps [rdx+8*(rax+8)], ymm2 ;3
+        vmovaps [rdx+8*(rax+12)], ymm3 ;4
 
         add rax, 16               ; i += 4*n (n=4)
         cmp rax, rdi
