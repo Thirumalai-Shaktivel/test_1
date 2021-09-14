afaab045a2a21b2641b5f7b6c0caae48d2320a41
Ondřej Čertík <ondrej@certik.us>
2021-09-14 15:19:42 -0600

    Update the notebook

--- Performance Vectorized.py
+++ Performance Vectorized.py
@@ -13,16 +13,27 @@
 #     name: python3
 # ---
 
-# !./build.sh
+# +
+# Supported CPUs for analysis:
+# 1 ... 2.4 GHz 8-Core Intel Core i9 (MacBook Pro 2019)
+# 2 ... Apple M1 (ARM)
+
+CPU = 1
+
+# +
+# Select one of the supported build platforms:
+# 1 ... Linux Intel 64
+# 2 ... macOS Intel 64
+# 3 ... macOS ARM 64
+
+# !./build.sh 2
 # # !./sin_perf > sin_data.txt
 # !./sin_perf_pure_vec > sin_pure_data_vec.txt
+# -
 
 # %pylab inline
 
 # +
-#D = loadtxt("sin_data.txt")
-#x = D[:,0]
-#sin_gf = D[:,2]
 D = loadtxt("sin_pure_data_vec.txt")
 x2 = D[:,0]
 sin_pure = D[:,2]
@@ -33,60 +44,84 @@ GHz = 1e9
 KB = 1024
 MB = 1024**2
 
-# Intel:
-# https://www.agner.org/optimize/instruction_tables.pdf
-# R: 0.125    (`VMOVAPS y,m256` 0.5 cycles per instruction = 4 doubles)
-# W: 0.25     (`VMOVAPS m256,y` 1 cycle per instruction = 4 doubles)
-# *: 0.125    (`vmulpd` is 0.5 cycles per 4 doubles)
-# # +: 0.125
-# fma: 0.125  (`VFMADD...` is 0.5 cycles)
-#cpu_freq = 2.4 * GHz # Base
-#cpu_freq = 5.3 * GHz # Boost
-cpu_freq = 4.590 * GHz # Actual
-L1 = 64 * KB
-L2 = 256 * KB
-L3 = 16 * MB
-k = 8 * 2  # 8 bytes per element, 2 arrays
-R_clock = 0.125
-W_clock = 0.25
+if CPU == 1:
+    # Intel:
+    # https://www.agner.org/optimize/instruction_tables.pdf
+    # R: 0.125    (`VMOVAPS y,m256` 0.5 cycles per instruction = 4 doubles)
+    # W: 0.25     (`VMOVAPS m256,y` 1 cycle per instruction = 4 doubles)
+    # *: 0.125    (`vmulpd` is 0.5 cycles per 4 doubles)
+    # +: 0.125
+    # fma: 0.125  (`VFMADD...` is 0.5 cycles)
+    #cpu_freq = 2.4 * GHz # Base
+    #cpu_freq = 5.3 * GHz # Boost
+    cpu_freq = 4.590 * GHz # Actual
+    L1 = 64 * KB
+    L2 = 256 * KB
+    L3 = 16 * MB
+    R_clock = 0.125
+    W_clock = 0.25
+    mul_clock = 0.125
+    plus_clock = 0.125
+    fma_clock = 0.125
+elif CPU == 2:
+    # ARM:
+    # https://www.techpowerup.com/cpu-specs/core-i9-10980hk.c2276
+    # https://dougallj.github.io/applecpu/firestorm-simd.html
+    #
+    # Operation speeds Apple M1 (ARM64) per double
+    #
+    # R: 0.1665 - 0.2 - 0.25 (`ldr q0, [x1]` takes 0.333)
+    # W: 0.25 - 0.333 - 0.5   (Both `stp d0, d1, [x1]` and `str q1, [x1]` take 0.5 cycles; `stp q1, q2, [x1]` takes 1 cycle)
+    # *: 0.125  (`fmul.2d v0, v0, v0` takes 0.25)
+    # +: 0.125  (`fadd.2d v0, v0, v0` takes 0.25)
+    # fma: 0.125  (`fmla.2d v0, v0, v0` takes 0.25)
+    #
+    # Note: There are two units for R, one unit for W and one unit can do both. The
+    # first number in 0.25 - 0.333 - 0.5 is only writing, then read/write sharing
+    # 50% of the common unit, and the last number is only the 1 unit for  W.
+    # Example: For array copy, we assume the unit gets used 50%, use the middle
+    # number and expect 0.333 for the peak.
+    #
+    #cpu_freq = 2.4 * GHz # Base
+    cpu_freq = 3.2 * GHz # Boost
+    L1 = 320 * KB
+    L2 = 12 * MB
+    L3 = None
+    R_clock = 0.1667
+    W_clock = 0.25
+    mul_clock = 0.125
+    plus_clock = 0.125
+    fma_clock = 0.125
+else:
+    raise Exception("CPU type not supported")
 
-# ARM:
-# https://www.techpowerup.com/cpu-specs/core-i9-10980hk.c2276
-# https://dougallj.github.io/applecpu/firestorm-simd.html
-#
-# Operation speeds Apple M1 (ARM64) per double
-#
-# R: 0.1665 - 0.2 - 0.25 (`ldr q0, [x1]` takes 0.333)
-# W: 0.25 - 0.333 - 0.5   (Both `stp d0, d1, [x1]` and `str q1, [x1]` take 0.5 cycles; `stp q1, q2, [x1]` takes 1 cycle)
-# *: 0.125  (`fmul.2d v0, v0, v0` takes 0.25)
-# # +: 0.125  (`fadd.2d v0, v0, v0` takes 0.25)
-# fma: 0.125  (`fmla.2d v0, v0, v0` takes 0.25)
-#
-# Note: There are two units for R, one unit for W and one unit can do both. The
-# first number in 0.25 - 0.333 - 0.5 is only writing, then read/write sharing
-# 50% of the common unit, and the last number is only the 1 unit for  W.
-# Example: For array copy, we assume the unit gets used 50%, use the middle
-# number and expect 0.333 for the peak.
-#
-#cpu_freq = 2.4 * GHz # Base
-cpu_freq = 3.2 * GHz # Boost
-L1 = 320 * KB
-L2 = 12 * MB
-#L3 = 16 * MB
+
+# Benchmark details:
 k = 8 * 2 # 8 bytes per element, 2 arrays
-R_clock = 0.1667
-W_clock = 0.25
+kernel_peak = 7*fma_clock + 2*mul_clock
+
+def draw_peak(x, L1_peak, L1, L2, L3, n, label, color):
+    L1x = L1 / (8*n)
+    L2x = L2 / (8*n)
+    if L3:
+        L3x = L3 / (8*n)
+    semilogx([x[0], L1x], [L1_peak, L1_peak], "--", lw=1, label=label, color=color)
+    semilogx([L1x, L2x], [L1_peak, L1_peak], "--", lw=2, color=color)
+    if L3:
+        semilogx([L2x, L3x], [L1_peak, L1_peak], "--", lw=3, color=color)
+        semilogx([L3x, x[-1]], [L1_peak, L1_peak], "--", lw=4, color=color)
+    else:
+        semilogx([L2x, x[-1]], [L1_peak, L1_peak], "--", lw=3, color=color)
+
+
 
 figure(figsize=(20, 12))
-#loglog(x, sin_gf, ".", label="GFortran Intrinsic")
-semilogx([x2[0], x2[-1]], [R_clock, R_clock], "--", color="gray")
-semilogx([x2[0], x2[-1]], [W_clock, W_clock], "--", color="gray")
+draw_peak(x2, kernel_peak, L1, L2, L3, 2, "Kernel Theoretical Peak Performance in L1", "g")
+draw_peak(x2, R_clock, L1, L2, L3, 1, "R Theoretical Peak Performance in L1", "gray")
+draw_peak(x2, W_clock, L1, L2, L3, 1, "W Theoretical Peak Performance in L1", "gray")
 semilogx(x2, read * cpu_freq, ".", label="R (0.125)")
 semilogx(x2, write * cpu_freq, ".", label="W (0.25)")
-semilogx(x2, sin_pure * cpu_freq, ".", label="Pure double double")
-semilogx([L1/k, L1/k], [0, 2], "-", label="L1")
-semilogx([L2/k, L2/k], [0, 2], "-", label="L2")
-#semilogx([L3/k, L3/k], [0, 2], "-", label="L3")
+semilogx(x2, sin_pure * cpu_freq, "g.", label="Kernel Actual")
 legend()
 xlabel("Array length [double]")
 ylabel("Time of sin(x) per array element")
@@ -113,8 +148,11 @@ print("CPU freq W avg: %.3f GHz" % (W_clock / Wa / GHz),
       "CPU freq W min: %.3f GHz" % (W_clock / Wm / GHz)
     )
 
-print("Using CPU freq: %.3f GHz" % (cpu_freq / GHz))
+print("Using CPU freq: \x1b[1m%.3f GHz\x1b[0m" % (cpu_freq / GHz))
 
 print("R avg: %.3f" % (Ra * cpu_freq), "R min: %.3f" % (Rm * cpu_freq))
 print("W avg: %.3f" % (Wa * cpu_freq), "W min: %.3f" % (Wm * cpu_freq))
-print("kernel min:", min(sin_pure * cpu_freq))
+kernel_min = min(sin_pure * cpu_freq)
+print("kernel min: %.3f" % kernel_min)
+print("kernel peak: %.3f" % kernel_peak)
+print("kernel percent peak: %.2f%%" % (kernel_peak / kernel_min * 100))
