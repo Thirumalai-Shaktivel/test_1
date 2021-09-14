72b3c991713f6a22c366e3dbf6b6690d585d2f16
Ondřej Čertík <ondrej@certik.us>
2021-09-14 14:31:25 -0600

    Make compilation work for macOS Intel

--- build.sh
+++ build.sh
@@ -1,40 +1,59 @@
 #!/bin/bash
 
-set -ex
+set -e
 
+
+if [[ $1 != "" ]]; then
+    platform=$1
+else
+    echo "Must specify platform."
+    echo "./build.sh NUM"
+    echo
+    echo "Supported platforms:"
+    echo "1 ... Linux Intel 64"
+    echo "2 ... macOS Intel 64"
+    echo "3 ... macOS ARM 64"
+    exit 1
+fi
+
+set -x
 rm -rf sin_perf_pure_vec
 
-# Use macho64 for macOS, win64 for Windows and elf64 for Linux
-#nasm -f macho64 array_copy2c.asm -o array_copy2.o
-as -c array_copy3a.asm -o array_copy2.o
-as -c array_copy3b.asm -o array_copy2.o
-as -c array_read2a.asm -o array_read.o
-as -c array_read2b.asm -o array_read.o
-as -c array_write2a.asm -o array_write.o
-as -c array_write2b.asm -o array_write.o
-as -c array_write2d.asm -o array_write.o
-as -c array_write2c.asm -o array_write.o
-as -c array_mul3a.asm -o array_mul.o
-as -c array_fma3a.asm -o array_fma.o
-#as -c kernel_sin5a.asm -o kernel_sin.o
-#as -c kernel_sin5b.asm -o kernel_sin.o
-#nasm -f macho64 array_read.asm
-#nasm -f macho64 array_write.asm
-#nasm -f macho64 kernel_sin1c.asm -o kernel_sin.o
-#clang -O2 -c kernel_sin2.ll -o kernel_sin.o
-#clang -O3 -funroll-loops -ffast-math -c kernel_sin3.c -o kernel_sin.o
-gfortran -O3 -march=native -funroll-loops -ffast-math -c kernel_sin4.f90 -o kernel_sin.o
-#clang -O1 -c array_copy2.c -o array_copy2.o
 
-#nasm -f macho64 array_copy2c.asm -o array_copy2.o
-#nasm -f macho64 array_read.asm
-#nasm -f macho64 array_write.asm
-
-#nasm -f macho64 kernel_sin1c.asm -o kernel_sin.o
-#clang -O2 -march=native -c kernel_sin2.ll -o kernel_sin.o
-#clang -O3 -march=native -funroll-loops -ffast-math -c kernel_sin3.c -o kernel_sin.o
-#gfortran -O3 -march=skylake -funroll-loops -ffast-math -c kernel_sin4.f90 -o kernel_sin.o
-#clang -c array_copy2.c -o array_copy2.o
+if [[ $platform == "3" ]]; then
+    as -c array_copy3a.asm -o array_copy2.o
+    as -c array_copy3b.asm -o array_copy2.o
+    as -c array_read2a.asm -o array_read.o
+    as -c array_read2b.asm -o array_read.o
+    as -c array_write2a.asm -o array_write.o
+    as -c array_write2b.asm -o array_write.o
+    as -c array_write2d.asm -o array_write.o
+    as -c array_write2c.asm -o array_write.o
+    as -c array_mul3a.asm -o array_mul.o
+    as -c array_fma3a.asm -o array_fma.o
+    #as -c kernel_sin5a.asm -o kernel_sin.o
+    #as -c kernel_sin5b.asm -o kernel_sin.o
+    gfortran -O3 -march=native -funroll-loops -ffast-math -c kernel_sin4.f90 -o kernel_sin.o
+elif [[ $platform == "2" || $platform == "1" ]]; then
+    # Use macho64 for macOS, win64 for Windows and elf64 for Linux
+    if [[ $platform == "1" ]]; then
+        nasm_f="elf64"
+    else
+        nasm_f="macho64"
+    fi
+    #clang -c array_copy2.c -o array_copy2.o
+    nasm -f $nasm_f array_copy2c.asm -o array_copy2.o
+    nasm -f $nasm_f array_read.asm
+    nasm -f $nasm_f array_write.asm
+    nasm -f $nasm_f kernel_sin1c.asm -o kernel_sin.o
+    clang -O2 -c kernel_sin2.ll -o kernel_sin.o
+    clang -O3 -funroll-loops -ffast-math -c kernel_sin3.c -o kernel_sin.o
+    #clang -O1 -c array_copy2.c -o array_copy2.o
+    nasm -f $nasm_f kernel_sin1c.asm -o kernel_sin.o
+    #clang -O2 -march=native -c kernel_sin2.ll -o kernel_sin.o
+    #clang -O3 -march=native -funroll-loops -ffast-math -c kernel_sin3.c -o kernel_sin.o
+    gfortran -O3 -march=skylake -funroll-loops -ffast-math -c kernel_sin4.f90 -o kernel_sin.o
+fi
 
 gfortran -O3 -funroll-loops -ffast-math sin_perf.f90 -o sin_perf
 
@@ -45,4 +64,4 @@ gfortran -O3 -funroll-loops -ffast-math -c sin_perf_pure_vec.f90 -o sin_perf_pur
 gfortran -O3 -funroll-loops -ffast-math -flto \
     -o sin_perf_pure_vec sin_perf_pure_vec.o sin_perf_pure_vec2.o \
     kernel_sin.o \
-    array_copy2.o array_read.o array_write.o array_mul.o array_fma.o
+    array_copy2.o array_read.o array_write.o # array_mul.o array_fma.o
