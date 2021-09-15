d2e67a95e022fb0c4d6e1db46b69231344fc22f6
Ondřej Čertík <ondrej@certik.us>
2021-09-15 15:58:53 -0600

    Check accuracy

--- sin_accuracy_pure.f90
+++ sin_accuracy_pure.f90
@@ -19,7 +19,7 @@ alpha = (xmax - xmin) / (exp(beta*N) - 1)
 
 do i = 1, N+1
     x = alpha * (exp(beta*(i-1)) - 1) + xmin
-    print "(es23.16, '   ', es25.16e3, '   ', es25.16e3)", x, dsin1(x), dsin2(x)
+    print "(es23.16, '   ', es25.16e3, '   ', es25.16e3)", x, dsin1(x), dsin3(x)
 end do
 
 contains
@@ -52,6 +52,15 @@ y = min(y, pi - y)
 y = max(y, -pi - y)
 y = min(y, pi - y)
 r = kernel_dsin(y)
+end function
+
+elemental real(dp) function dsin3(x) result(r)
+real(dp), intent(in) :: x
+real(dp) :: y
+real(dp) :: A(1), B(1)
+A(1) = x
+call kernel_sin1(1_8, A, B)
+r = B(1)
 end function
 
 ! Accurate on [-pi/2,pi/2] to about 1e-16
@@ -241,6 +250,54 @@ if (abs(xh) < 1e16) then
 else
     error stop "unsupported range"
 end if
+end function
+
+pure subroutine kernel_sin1(n, A, B) bind(c)
+! Intel: 1.23 cycles per double; peak: 9*0.125 = 1.125
+! ARM: 1.23 cycles per double
+use iso_fortran_env, only: dp=>real64
+use iso_c_binding, only: c_long, c_double
+integer(c_long), value, intent(in) :: n
+real(c_double), intent(in) :: A(n)
+real(c_double), intent(out) :: B(n)
+real(dp), parameter :: S1 = 0.9999999999999990771_dp
+real(dp), parameter :: S2 = -0.16666666666664811048_dp
+real(dp), parameter :: S3 = 8.333333333226519387e-3_dp
+real(dp), parameter :: S4 = -1.9841269813888534497e-4_dp
+real(dp), parameter :: S5 = 2.7557315514280769795e-6_dp
+real(dp), parameter :: S6 = -2.5051823583393710429e-8_dp
+real(dp), parameter :: S7 = 1.6046585911173017112e-10_dp
+real(dp), parameter :: S8 = -7.3572396558796051923e-13_dp
+real(dp) :: x, z
+integer(c_long) :: i
+do i = 1, n
+    x = A(i)
+    x = modulo_2pi_6b(x)
+    x = min(x, pi - x)
+    x = max(x, -pi - x)
+    x = min(x, pi - x)
+    z = x*x
+    B(i) = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
+end do
+
+end subroutine
+
+pure real(c_double) function modulo_2pi_6b(xh) result(zh)
+use iso_c_binding, only: c_long, c_double
+real(c_double), intent(in) :: xh
+integer(c_long) :: N
+real(c_double) :: yh, yl, Nd, p1, p2, p3, p4, p5, p6, p7, p8
+yh = 6.283185307179586_dp ! 2*pi (high)
+p1 = 6.25000000000000000e+00_dp
+p2 = 3.12500000000000000e-02_dp
+p3 = 1.89208984375000000e-03_dp
+p4 = 4.19616699218750000e-05_dp
+p5 = 1.25169754028320312e-06_dp
+p6 = 3.95812094211578369e-09_dp
+p7 = 1.00044417195022106e-11_dp
+p8 = 2.48934886875864535e-13_dp
+Nd = floor(xh/yh)
+zh = (((((((xh - Nd*p1) - Nd*p2) - Nd*p3) - Nd*p4) - Nd*p5) - Nd*p6) - Nd*p7) - Nd*p8
 end function
 
 end program
