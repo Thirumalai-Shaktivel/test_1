1a24a37ff2dc26b4cd58c0e4e550e37d9b2e0e40
Ondřej Čertík <ondrej@certik.us>
2021-09-15 16:04:46 -0600

    Accuracy still good

--- sin_accuracy_pure.f90
+++ sin_accuracy_pure.f90
@@ -268,30 +268,22 @@ real(dp), parameter :: S5 = 2.7557315514280769795e-6_dp
 real(dp), parameter :: S6 = -2.5051823583393710429e-8_dp
 real(dp), parameter :: S7 = 1.6046585911173017112e-10_dp
 real(dp), parameter :: S8 = -7.3572396558796051923e-13_dp
-real(dp) :: x, z
+real(dp), parameter :: twopi = 6.283185307179586_dp
+real(dp), parameter :: p1 = 6.28318405151367188e+00_dp
+real(dp), parameter :: p2 = 1.25566566566703841e-06_dp
+real(dp), parameter :: p3 = 2.48934886875864535e-13_dp
+real(dp) :: x, z, Nd
 integer(c_long) :: i
 do i = 1, n
     x = A(i)
-    x = modulo_2pi_6b(x)
+    Nd = floor(x/twopi)
+    x = ((x - Nd*p1) - Nd*p2) - Nd*p3
     x = min(x, pi - x)
     x = max(x, -pi - x)
     x = min(x, pi - x)
     z = x*x
     B(i) = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
 end do
-
 end subroutine
-
-pure real(c_double) function modulo_2pi_6b(xh) result(zh)
-use iso_c_binding, only: c_double
-real(c_double), intent(in) :: xh
-real(c_double) :: yh, Nd, p1, p2, p3
-yh = 6.283185307179586_dp
-p1 = 6.28318405151367188e+00_dp
-p2 = 1.25566566566703841e-06_dp
-p3 = 2.48934886875864535e-13_dp
-Nd = floor(xh/yh)
-zh = ((xh - Nd*p1) - Nd*p2) - Nd*p3
-end function
 
 end program
