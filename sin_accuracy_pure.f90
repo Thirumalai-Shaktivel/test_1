8f5a03bb5d77c22a6a8e2b3339a4284c1342d20c
Ondřej Čertík <ondrej@certik.us>
2021-09-15 11:56:22 -0600

    Implement accurate modulo

--- sin_accuracy_pure.f90
+++ sin_accuracy_pure.f90
@@ -47,7 +47,7 @@ end function
 elemental real(dp) function dsin2(x) result(r)
 real(dp), intent(in) :: x
 real(dp) :: y
-y = modulo_2pi(x)
+y = modulo_2pi_4(x)
 y = min(y, pi - y)
 y = max(y, -pi - y)
 y = min(y, pi - y)
@@ -116,6 +116,85 @@ if (abs(xh) < 1e16) then
     N = floor2(xh/yh)
     call dd_mul(zh, zl, -real(N,dp), 0._dp, yh, yl)
     zh = dd_add1(xh, zh, zl)
+else
+    error stop "unsupported range"
+end if
+end function
+
+pure real(dp) function modulo_2pi_2(xh) result(zh)
+real(dp), intent(in) :: xh
+integer(8) :: N
+real(dp) :: yh, yl, Nd, p1, p2, p3
+if (abs(xh) < 1e16) then
+    yh = 6.283185307179586_dp ! 2*pi (high)
+    p1 = 6.283184051513672
+    p2 = 1.2556656656670384e-06
+    p3 = 2.4893488687586454e-13
+    N = floor2(xh/yh)
+    Nd = real(N,dp)
+    zh = ((xh - Nd*p1) - Nd*p2) - Nd*p3
+else
+    error stop "unsupported range"
+end if
+end function
+
+pure real(dp) function modulo_2pi_3(xh) result(zh)
+real(dp), intent(in) :: xh
+integer(8) :: N
+real(dp) :: yh, yl, Nd, p1, p2, p3, p4, p5
+if (abs(xh) < 1e16) then
+    yh = 6.283185307179586_dp ! 2*pi (high)
+    p1 = 6.28125000000000000e+00
+    p2 = 1.93405151367187500e-03
+    p3 = 1.25542283058166504e-06
+    p4 = 2.42835085373371840e-10
+    p5 = 2.48934886875864535e-13
+    N = floor2(xh/yh)
+    Nd = real(N,dp)
+    zh = ((((xh - Nd*p1) - Nd*p2) - Nd*p3) - Nd*p4) - Nd*p5
+else
+    error stop "unsupported range"
+end if
+end function
+
+pure real(dp) function modulo_2pi_4(xh) result(zh)
+real(dp), intent(in) :: xh
+integer(8) :: N
+real(dp) :: yh, yl, Nd, p1, p2, p3, p4, p5, p6, p7, p8, p9, p10, p11, p12, &
+    p13, p14, p15, p16, p17, p18, p19, p20, p21, p22, p23, p24
+if (abs(xh) < 1e16) then
+    yh = 6.283185307179586_dp ! 2*pi (high)
+p1 = 4.00000000000000000e+00_dp
+p2 = 2.00000000000000000e+00_dp
+p3 = 2.50000000000000000e-01_dp
+p4 = 3.12500000000000000e-02_dp
+p5 = 9.76562500000000000e-04_dp
+p6 = 4.88281250000000000e-04_dp
+p7 = 2.44140625000000000e-04_dp
+p8 = 1.22070312500000000e-04_dp
+p9 = 6.10351562500000000e-05_dp
+p10 = 3.05175781250000000e-05_dp
+p11 = 7.62939453125000000e-06_dp
+p12 = 3.81469726562500000e-06_dp
+p13 = 9.53674316406250000e-07_dp
+p14 = 2.38418579101562500e-07_dp
+p15 = 5.96046447753906250e-08_dp
+p16 = 3.72529029846191406e-09_dp
+p17 = 2.32830643653869629e-10_dp
+p18 = 7.27595761418342590e-12_dp
+p19 = 1.81898940354585648e-12_dp
+p20 = 9.09494701772928238e-13_dp
+p21 = 2.27373675443232059e-13_dp
+p22 = 1.42108547152020037e-14_dp
+p23 = 7.10542735760100186e-15_dp
+p24 = 2.44929359829470641e-16_dp
+    N = floor2(xh/yh)
+    Nd = real(N,dp)
+    zh = (((((((((((((((((((((((xh - Nd*p1) - Nd*p2) - Nd*p3) - Nd*p4) &
+        - Nd*p5) - Nd*p6) - Nd*p7) - Nd*p8) - Nd*p9) - Nd*p10) - Nd*p11) &
+        - Nd*p12) - Nd*p13) - Nd*p14) - Nd*p15) - Nd*p16) - Nd*p17) &
+        - Nd*p18) - Nd*p19) - Nd*p20) - Nd*p21) - Nd*p22) - Nd*p23) &
+        - Nd*p24
 else
     error stop "unsupported range"
 end if
