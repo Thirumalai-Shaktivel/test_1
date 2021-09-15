d0d67c1f8f3852e9a23416c51ee8eadc22c118e8
Ondřej Čertík <ondrej@certik.us>
2021-09-15 14:55:24 -0600

    Add modulo to performance

--- kernel_sin4.f90
+++ kernel_sin4.f90
@@ -18,7 +18,32 @@ real(dp) :: x, z
 integer(c_long) :: i
 do i = 1, n
     x = A(i)
+    x = modulo_2pi_6(x)
+    x = min(x, pi - x)
+    x = max(x, -pi - x)
+    x = min(x, pi - x)
     z = x*x
     B(i) = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
 end do
+
+contains
+
+pure real(c_double) function modulo_2pi_6(xh) result(zh)
+real(c_double), intent(in) :: xh
+integer(c_long) :: N
+real(c_double) :: yh, yl, Nd, p1, p2, p3, p4, p5, p6, p7, p8
+yh = 6.283185307179586_dp ! 2*pi (high)
+p1 = 6.25000000000000000e+00_dp
+p2 = 3.12500000000000000e-02_dp
+p3 = 1.89208984375000000e-03_dp
+p4 = 4.19616699218750000e-05_dp
+p5 = 1.25169754028320312e-06_dp
+p6 = 3.95812094211578369e-09_dp
+p7 = 1.00044417195022106e-11_dp
+p8 = 2.48934886875864535e-13_dp
+N = floor(xh/yh)
+Nd = real(N,dp)
+zh = (((((((xh - Nd*p1) - Nd*p2) - Nd*p3) - Nd*p4) - Nd*p5) - Nd*p6) - Nd*p7) - Nd*p8
+end function
+
 end subroutine
