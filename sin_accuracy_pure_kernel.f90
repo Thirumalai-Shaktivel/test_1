0078aebfddf9969cf0997f0b5029367156a3cf13
Ondřej Čertík <ondrej@certik.us>
2021-09-15 13:29:25 -0600

    Add a kernel

--- sin_accuracy_pure_kernel.f90
+++ sin_accuracy_pure_kernel.f90
@@ -30,6 +30,37 @@ y = min(y, pi - y)
 r = kernel_dsin(y)
 end function
 
+elemental integer(8) function floor2(x) result(r)
+real(dp), intent(in) :: x
+if (x >= 0) then
+    r = x
+else
+    r = x-1
+end if
+end function
+
+pure real(dp) function modulo_2pi_6(xh) result(zh)
+real(dp), intent(in) :: xh
+integer(8) :: N
+real(dp) :: yh, yl, Nd, p1, p2, p3, p4, p5, p6, p7, p8
+if (abs(xh) < 1e16) then
+    yh = 6.283185307179586_dp ! 2*pi (high)
+    p1 = 6.25000000000000000e+00_dp
+    p2 = 3.12500000000000000e-02_dp
+    p3 = 1.89208984375000000e-03_dp
+    p4 = 4.19616699218750000e-05_dp
+    p5 = 1.25169754028320312e-06_dp
+    p6 = 3.95812094211578369e-09_dp
+    p7 = 1.00044417195022106e-11_dp
+    p8 = 2.48934886875864535e-13_dp
+    N = floor2(xh/yh)
+    Nd = real(N,dp)
+    zh = (((((((xh - Nd*p1) - Nd*p2) - Nd*p3) - Nd*p4) - Nd*p5) - Nd*p6) - Nd*p7) - Nd*p8
+else
+    error stop "unsupported range"
+end if
+end function
+
 ! Accurate on [-pi/2,pi/2] to about 1e-16
 elemental real(dp) function kernel_dsin(x) result(res)
 real(dp), intent(in) :: x
@@ -41,6 +72,23 @@ real(dp), parameter :: S5 = 2.7557315514280769795e-6_dp
 real(dp), parameter :: S6 = -2.5051823583393710429e-8_dp
 real(dp), parameter :: S7 = 1.6046585911173017112e-10_dp
 real(dp), parameter :: S8 = -7.3572396558796051923e-13_dp
+
+real(dp) :: z
+z = x*x
+res = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
+end function
+
+! Accurate on [-pi/2,pi/2] to about 1e-16
+elemental real(dp) function kernel_dsin2(x) result(res)
+real(dp), intent(in) :: x
+real(dp), parameter :: S1 = 1
+real(dp), parameter :: S2 = -0.16666666666665748417_dp
+real(dp), parameter :: S3 = 8.333333333260810195e-3_dp
+real(dp), parameter :: S4 = -1.9841269819408224684e-4_dp
+real(dp), parameter :: S5 = 2.7557315969010714494e-6_dp
+real(dp), parameter :: S6 = -2.5051843446312301534e-8_dp
+real(dp), parameter :: S7 = 1.6047020166520616231e-10_dp
+real(dp), parameter :: S8 = -7.360938387054769116e-13_dp
 real(dp) :: z
 z = x*x
 res = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
