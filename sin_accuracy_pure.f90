52086e9b95dd9c57a109bed5f4fa6c54be057ea8
Ondřej Čertík <ondrej@certik.us>
2021-09-19 14:49:29 -0600

    Faster version

--- sin_accuracy_pure.f90
+++ sin_accuracy_pure.f90
@@ -387,6 +387,7 @@ end subroutine
 
 
 subroutine kernel_sin4(n, A, B) bind(c)
+! Intel: runs at 1.545 cycles; Peak:: 1.458
 use iso_fortran_env, only: dp=>real64
 use iso_c_binding, only: c_long, c_double
 implicit none
@@ -396,12 +397,13 @@ real(c_double), intent(out) :: B(n)
 real(dp), parameter :: S1 =  0.982396485658623
 real(dp), parameter :: S2 = -0.14013802346642243
 real(dp), parameter :: pi = 3.1415926535897932384626433832795_dp
+real(dp), parameter :: one_over_pi = 1/pi
 real(dp) :: x, z, Nd
 integer(c_long) :: i, xi
 equivalence (x,xi)
 do i = 1, n
     x = A(i)
-    Nd = nint(x/pi)
+    Nd = int(x*one_over_pi + 0.5_dp)
     x = x - Nd*pi
     ! -pi/2 < x < pi/2
     ! For even Nd, we have sin(A(i)) = sin(x)
