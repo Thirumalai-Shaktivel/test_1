be64c2ba2a9ec3b42292ca6860c581f294dac1ba
Ondřej Čertík <ondrej@certik.us>
2021-09-26 18:47:14 -0600

    Add more accurate kernel

--- sin_accuracy_pure.f90
+++ sin_accuracy_pure.f90
@@ -65,6 +65,15 @@ call kernel_sin3(1_8, A, B)
 r = B(1)
 end function
 
+real(dp) function dsin3b(x) result(r)
+real(dp), intent(in) :: x
+real(dp) :: y
+real(dp) :: A(1), B(1)
+A(1) = x
+call kernel_sin3b(1_8, A, B)
+r = B(1)
+end function
+
 real(dp) function dsin4(x) result(r)
 real(dp), intent(in) :: x
 real(dp) :: y
@@ -396,6 +405,40 @@ do i = 1, n
     x = B(i)
     z = x*x
     B(i) = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
+!    B(i) = kernel_dsin(x)
+end do
+end subroutine
+
+subroutine kernel_sin3b(n, A, B) bind(c)
+use iso_fortran_env, only: dp=>real64
+use iso_c_binding, only: c_long, c_double
+implicit none
+integer(c_long), value, intent(in) :: n
+real(c_double), intent(in) :: A(n)
+real(c_double), intent(out) :: B(n)
+real(dp), parameter :: p1 = 3.14159202575683594e+00_dp
+real(dp), parameter :: p2 = 6.27832832833519205e-07_dp
+real(dp), parameter :: p3 = 1.24467443437932268e-13_dp
+real(dp), parameter :: pi = 3.1415926535897932384626433832795_dp
+real(dp), parameter :: one_over_pi = 1/pi
+real(dp) :: x, z, Nd
+integer(c_long) :: i, xi
+equivalence (x,xi)
+do i = 1, n
+    x = A(i)
+    !Nd = nint(x*one_over_pi)
+    Nd = int(x/pi + 0.5_dp*sign(1._dp, x))
+    x = ((x - Nd*p1) - Nd*p2) - Nd*p3
+    ! -pi/2 < x < pi/2
+    ! For even Nd, we have sin(A(i)) = sin(x)
+    ! For odd Nd,  we have sin(A(i)) = sin(x+pi) = -sin(x) = sin(-x)
+    !if (modulo(int(Nd), 2) == 1) x = -x
+    !if (and(int(Nd), 1) /= 0) x = -x
+    xi = xor(shiftl(int(Nd, c_long),63), xi)
+    B(i) = x
+end do
+do i = 1, n
+    B(i) = kernel_dsin(B(i))
 end do
 end subroutine
 
