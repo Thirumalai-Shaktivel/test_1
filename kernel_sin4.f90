a382b907a9e93270c11ad8b8f4a93bce07f859bd
Ondřej Čertík <ondrej@certik.us>
2021-09-26 19:01:28 -0600

    Update timings

--- kernel_sin4.f90
+++ kernel_sin4.f90
@@ -1,5 +1,5 @@
 subroutine kernel_sin1(n, A, B) bind(c)
-! Intel: 3.65 cycles per double; peak: 2.5
+! Intel: 2.83 cycles per double; peak: 2.458
 ! ARM: 3.26 cycles per double; peak: 2.25
 use iso_fortran_env, only: dp=>real64
 use iso_c_binding, only: c_long, c_double
