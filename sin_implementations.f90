64de4ac768134c069fd95f7a9a552a9c3f6656a9
Ondřej Čertík <ondrej@certik.us>
2021-11-12 08:53:36 -0700

    Revert "remove repeating function definition"
    This reverts commit 61eeb5499de427a56c3533f3e7838c0bdd6e1f10.

--- sin_implementations.f90
+++ sin_implementations.f90
@@ -35,7 +35,7 @@ real(dp) function dsin3b(x) result(r)
 real(dp), intent(in) :: x
 real(dp) :: A(1), B(1)
 A(1) = x
-call kernel_sin1(1_8, A, B)
+call kernel_sin3b(1_8, A, B)
 r = B(1)
 end function
 
@@ -62,6 +62,29 @@ real(dp) :: z
 z = x*x
 res = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
 end function
+
+subroutine kernel_sin3b(n, A, B) bind(c)
+implicit none
+integer(i8), value, intent(in) :: n
+real(dp), intent(in) :: A(n)
+real(dp), intent(out) :: B(n)
+real(dp), parameter :: p1 = 3.14159202575683594e+00_dp
+real(dp), parameter :: p2 = 6.27832832833519205e-07_dp
+real(dp), parameter :: p3 = 1.24467443437932268e-13_dp
+real(dp) :: x, Nd
+integer(i8) :: i, xi
+equivalence (x,xi)
+do i = 1, n
+    x = A(i)
+    Nd = int(x/pi + 0.5_dp*sign(1._dp, x))
+    x = ((x - Nd*p1) - Nd*p2) - Nd*p3
+    xi = xor(shiftl(int(Nd, i8),63), xi)
+    B(i) = x
+end do
+do i = 1, n
+    B(i) = kernel_dsin(B(i))
+end do
+end subroutine
 
 subroutine kernel_sin4(n, A, B) bind(c)
 ! Intel: runs at 1.545 cycles; Peak: 1.458
@@ -103,8 +126,9 @@ real(dp), intent(out) :: B(n)
 real(dp), parameter :: p1 = 3.14159202575683594e+00_dp
 real(dp), parameter :: p2 = 6.27832832833519205e-07_dp
 real(dp), parameter :: p3 = 1.24467443437932268e-13_dp
-! real(dp), parameter :: one_over_pi = 1/pi
-real(dp) :: x, Nd
+real(dp), parameter :: pi = 3.1415926535897932384626433832795_dp
+real(dp), parameter :: one_over_pi = 1/pi
+real(dp) :: x, z, Nd
 integer(i8) :: i, xi
 equivalence (x,xi)
 do i = 1, n
@@ -123,6 +147,24 @@ end do
 do i = 1, n
     B(i) = kernel_dsin(B(i))
 end do
+
+contains
+
+    ! Accurate on [-pi/2,pi/2] to about 1e-16
+    elemental real(dp) function kernel_dsin(x) result(res)
+    real(dp), intent(in) :: x
+    real(dp), parameter :: S1 = 1
+    real(dp), parameter :: S2 = -0.16666666666665748417_dp
+    real(dp), parameter :: S3 = 8.333333333260810195e-3_dp
+    real(dp), parameter :: S4 = -1.9841269819408224684e-4_dp
+    real(dp), parameter :: S5 = 2.7557315969010714494e-6_dp
+    real(dp), parameter :: S6 = -2.5051843446312301534e-8_dp
+    real(dp), parameter :: S7 = 1.6047020166520616231e-10_dp
+    real(dp), parameter :: S8 = -7.360938387054769116e-13_dp
+    real(dp) :: z
+    z = x*x
+    res = x * (S1+z*(S2+z*(S3+z*(S4+z*(S5+z*(S6+z*(S7+z*S8)))))))
+    end function
 
 end subroutine
 
